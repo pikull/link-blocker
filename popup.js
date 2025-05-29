c32f0f48e7129461f1712ca608d6b8bdf997763d
lzhangcs <142354778+maxh119Z@users.noreply.github.com>
2025-05-29 13:49:43 -0700

    addshit

--- popup.js
+++ popup.js
@@ -1,7 +1,22 @@
+var textboxes = 0;
+
 document.getElementById("add").addEventListener("click",function(){
-    alert("hi")
+    textboxes++;
+    
     let input1 = document.createElement("input")
+    input1.id = "input1"+textboxes
+    let div = document.createElement("div");
+
+    let but = document.createElement("button");
+    but.innerHTML = "x";
+    but.addEventListener("click",function(){
+        div.remove()
+    })
+    div.className = "wrapper";
+
     input1.type = "text";
     input1.value = "HELLO";
-    document.body.appendChild(input1);
+    div.appendChild(input1)
+    div.appendChild(but)
+    document.body.appendChild(div);
 })
