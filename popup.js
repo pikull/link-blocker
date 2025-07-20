34cf4461d7da3faa7730088af61b95ebebadb14a
pikull <118777590+pikull@users.noreply.github.com>
2025-07-20 00:33:39 -0700

    format

--- popup.js
+++ popup.js
@@ -1,30 +1,32 @@
 var array = [];
 
-document.getElementById("add").addEventListener("click",function(){
+document.getElementById("add").addEventListener("click", function() {
     let value = document.getElementById("site").value;
-    if (value == "" || value == null || array.includes(value)){return}
-    let text = document.createElement("p")
+    if (value == "" || value == null || array.includes(value)) {
+        return;
+    }
+    let text = document.createElement("p");
     text.innerHTML = value;
 
-    array.push(value)
+    array.push(value);
     let div = document.createElement("div");
 
     let but = document.createElement("button");
     but.innerHTML = "x";
-    but.addEventListener("click",function(){
+    but.addEventListener("click", function() {
         let textremove = text.innerHTML;
         let index = array.indexOf(textremove);
         if (index > -1) {
             array.splice(index, 1);
         }
-        div.remove()
-    })
+        div.remove();
+    });
     div.className = "wrapper";
-    div.appendChild(text)
-    div.appendChild(but)
+    div.appendChild(text);
+    div.appendChild(but);
     document.body.appendChild(div);
-})
+});
 
-document.getElementById("update").addEventListener("click", function(){
+document.getElementById("update").addEventListener("click", function() {
     alert(array.toString());
-})
+});
